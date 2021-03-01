93bd0fff3d65e446367cd199e992b7353029fe59
EC2 Default User <ec2-user@ip-172-31-23-94.ec2.internal>
2021-03-01 13:13:56 +0000

    [Start,Finish]ア プリ２：9章

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,10 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
