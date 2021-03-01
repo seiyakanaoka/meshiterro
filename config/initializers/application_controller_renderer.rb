b5cbdd58a5d3b049c5e3f5e460c90cef42693b7b
EC2 Default User <ec2-user@ip-172-31-23-94.ec2.internal>
2021-03-01 07:28:39 +0000

    [Start]アプリ２８章

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '80fa339a1ac9cebbbba7cf77d544e1c731d6a77f6a66fa03ce2c0b526d5e71a3a71dbd3818c6f071b2e120c65dfb9344167eac4565296c44e42b7e3bdce6ce02'
